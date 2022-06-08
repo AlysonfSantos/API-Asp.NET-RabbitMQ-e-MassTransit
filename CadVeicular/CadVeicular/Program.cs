3fad06e962739c8973b93fb7cb12c3e1b4c25739
Alyson Diego Fagundes dos Santos <87543879+AlysonfSantos@users.noreply.github.com>
2022-06-08 15:16:56 -0300

    Configuração do RabbitMq

--- CadVeicular/CadVeicular/Program.cs
+++ CadVeicular/CadVeicular/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMassTransit(bus =>
+{ 
+    bus.UsingRabbitMq((ctx, busConfigurator)=>
+    {
+        busConfigurator.Host(builder.Configuration.GetConnectionString("RabbitMq"));
+    });
+});
+builder.Services.AddOptions<MassTransitHostOptions>().Configure(options =>
+{
+    // if specified, waits until the bus is started before
+    // returning from IHostedService.StartAsync
+    // default is false
+    options.WaitUntilStarted = true;
+
+    // if specified, limits the wait time when starting the bus
+    options.StartTimeout = TimeSpan.FromSeconds(10);
+
+    // if specified, limits the wait time when stopping the bus
+    options.StopTimeout = TimeSpan.FromSeconds(30);
+});
+
 
 var app = builder.Build();
 
